dec5ca8d7b300a3a1d7c4e8bad2f486212f00c4f
mehedyPUST <mehedy.pust@gmail.com>
2026-02-15 13:31:46 +0600

    partial practice done

---
+++ EventsJS/js/events.js
@@ -0,0 +1,11 @@
+console.log('External js');
+
+// option 2 for event handeling 
+function makeYellow() {
+    document.body.style.backgroundColor = 'yellow';
+
+}
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+
+}

b6e9518abb655dc71149f7276ceb5397b628d97f
mehedyPUST <mehedy.pust@gmail.com>
2026-02-15 19:11:34 +0600

    23-3 vide done

--- EventsJS/js/events.js
+++ EventsJS/js/events.js
@@ -9,3 +9,35 @@ function makeRed() {
     document.body.style.backgroundColor = 'red';
 
 }
+
+
+// 2nd method get element by id and thn set on click
+const btnMakeBlue = document.getElementById('btn-blue');
+// console.log(btnMakeBlue);
+btnMakeBlue.onclick = function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+
+}
+
+// method 3 
+
+const btnMakePurple = document.getElementById('btn-purple');
+// console.log(makePurple);
+btnMakePurple.onclick = makePurple;
+function makePurple() {
+    document.body.style.backgroundColor = 'purple';
+}
+
+
+// method 4 : addEventListener use kore 
+
+// document.getElementById().addEventListener('event type', handler)
+document.getElementById('btn-make-green')
+    .addEventListener('click', function makeGreen() {
+        document.body.style.backgroundColor = 'green';
+    })
+
+document.getElementById('btn-make-goldenrod')
+    .addEventListener('click', function () {
+        document.body.style.backgroundColor = 'goldenrod';
+    })
